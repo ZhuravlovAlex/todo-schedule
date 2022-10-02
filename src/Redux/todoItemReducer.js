2dad90821b8f75380f41affd83f7274ae8feb021
ZhuravlovAlex <zhuravlov.alex75@gmail.com>
2022-10-02 18:31:33 +0300

    feat: add types & actions, install typescript

--- src/Redux/todoItemReducer.js
+++ src/Redux/todoItemReducer.js
@@ -1,7 +1,15 @@
+import { CREATE_ITEM } from "./Types"
+
 const initialState = {
 	todoItem: []
 }
 
 export const todoItemReducer = (state = initialState, action) => {
-	return state
+	switch (action.type) {
+		case CREATE_ITEM:
+			return { ...state, todoItem: state.todoItem.concat(action.payload)}
+
+		default: return state
+	}
+
 }
